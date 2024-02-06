f9ae6c7bf6a270ae4aa42199d69af7b45f567ff4
Reidner Willians Pacheco Peres <pacheco195920@gmail.com>
2024-02-06 19:18:36 -0300

    css

--- Script/script.js
+++ Script/script.js
@@ -1,7 +1,10 @@
-const section = document.querySelector(".section");
+const wrapper = document.querySelector(".wrapper");
+const backBtn = document.querySelector(".back-btn");
 const menuBtn = document.querySelector(".menu-btn");
 
+
 const toggleScreen = () => {
-    section.classList.toggle("mostrar-tarefas");
+    Wrapper.classList.toggle("show-category");
 };
 menuBtn.addEventListener("click", toggleScreen);
+backBtn.addEventListener("click", toggleScreen);
